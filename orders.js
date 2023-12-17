e8441ecf4449d1041bcc094118a08c6504eb4bb4
kweku76 <daffran@gmail.com>
2023-12-17 20:55:19 +1000

    first commit

---
+++ orders.js
@@ -0,0 +1,31 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName: 'LAVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+
+    },
+    {
+        productName: 'ORANGE KF102 Drone',
+        productNumber: '26378',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '99374',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+
+    },
+
+]
